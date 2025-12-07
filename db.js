9b25a99d9c0cc3fcc86ec0f28490452a41ed12e3
TacoGlz <axeldlg1305@gmail.com>
2025-12-07 17:07:25 -0600

    cluster

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import  mongoose  from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/terceroa"
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/novenoa"
 
 export const connectDB = async () => {
   try {
