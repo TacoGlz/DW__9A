f6dd726ed6e25fe5ee7be817aae7862cff958981
TacoGlz <axeldlg1305@gmail.com>
2025-12-07 16:14:39 -0600

    Endpoints

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./db.js";
-import { Card } from "./models/Card.js";
+import { Card } from "./models/Cards.js";
 const app = express();
 app.use(express.json());
 app.use(cors());
